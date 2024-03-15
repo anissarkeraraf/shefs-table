359716dbc5fd8cbd308e3b4bfe1800784859435c
anissarkeraraf <anissarkeraraf@gmail.com>
2024-03-16 00:46:28 +0600

    added the all section

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Navber from './Navber/Navber'
+
+function App() {
+
+  return (
+    <>
+      <Navber></Navber>
+
+      <div>
+        <img className='w-full relative' src="../src/assets/Rectangle 1 (1).png" alt="" />
+        <div className='absolute bottom-48 text-center'>
+        <h1 className='text-4xl font-bold text-white'>Discover an exceptional cooking class tailored for you!</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
